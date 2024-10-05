2fcb1e581869e8b7ee4ee696fcc6705217d22826
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-10-05 13:03:51 +0500

    Add files via upload

---
+++ 5-10-2024/script.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+// Functions 
+
+// function hello(){
+// console.log("This Is Functions")
+// } 
+// hello();
+
+
+// Arrow Functions
+
+// var fnc = (hello) =>{
+//     console.log("Hello");
+// } 
+// fnc()
+
+// Annoynums Function
+
+// (function(){
+//     console.log("Hello World");
+// })()
