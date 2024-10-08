77d5f2086406b6329381793f0e8aaf0ff11ef956
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-10-08 13:26:39 +0500

    Add files via upload

---
+++ 8-10-2024/script.js
@@ -0,0 +1,61 @@
+
+// var sum = (a,b) => {
+//     return a*b;
+// };
+// console.log(sum(2,9));
+
+// var str = (a) => {
+//     return a;
+    
+// }
+// console.log(str("Hello World"))
+
+
+// var colors = ["red","green","blue","yellow","purple"];
+// var newArr = []
+// newArr = colors.slice(1,4);
+// console.log(newArr);
+
+var StudentData = {
+    name : "Arzoo Shamim",
+    age : 14,
+    rollNo : 257774,
+    perants : {
+     father : "Muhammad Shamim Uddin",
+     mother : "Ambri Bano"
+    },
+    siblings : {
+        s1 : "Muhammad Hussain",
+        s1 : "Muhammad Hussnain",
+        s2 : "Ronaq Shamim" ,
+        s3 : "Muntaha  Shamim" ,
+        s4 : "Muhammad Hunain" 
+    }
+
+};
+console.log(StudentData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
