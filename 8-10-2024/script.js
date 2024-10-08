6bc8fe7f978a18906194393dd161f000073a2e04
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-10-08 13:32:26 +0500

    Update script.js

--- 8-10-2024/script.js
+++ 8-10-2024/script.js
@@ -16,24 +16,24 @@
 // newArr = colors.slice(1,4);
 // console.log(newArr);
 
-var StudentData = {
-    name : "Arzoo Shamim",
-    age : 14,
-    rollNo : 257774,
-    perants : {
-     father : "Muhammad Shamim Uddin",
-     mother : "Ambri Bano"
-    },
-    siblings : {
-        s1 : "Muhammad Hussain",
-        s1 : "Muhammad Hussnain",
-        s2 : "Ronaq Shamim" ,
-        s3 : "Muntaha  Shamim" ,
-        s4 : "Muhammad Hunain" 
-    }
+// var StudentData = {
+//     name : "Arzoo Shamim",
+//     age : 14,
+//     rollNo : 257774,
+//     perants : {
+//      father : "Muhammad Shamim Uddin",
+//      mother : "Ambri Bano"
+//     },
+//     siblings : {
+//         s1 : "Muhammad Hussain",
+//         s1 : "Muhammad Hussnain",
+//         s2 : "Ronaq Shamim" ,
+//         s3 : "Muntaha  Shamim" ,
+//         s4 : "Muhammad Hunain" 
+//     }
 
-};
-console.log(StudentData);
+// };
+// console.log(StudentData);
 
 
 
